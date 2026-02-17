184a288ea183e2694ab7d887311f1e987de511f5
Jinn Of Maddnezz <147605568+JinnOfMaddnezz147@users.noreply.github.com>
2026-02-17 15:31:08 +0530

    Delete resume_collector directory

--- resume_collector/candidates/urls.py
+++
@@ -1,8 +0,0 @@
-# candidates/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/', views.upload_resume, name='upload_resume'),
-    path('', views.candidate_list, name='candidate_list'),
-]
